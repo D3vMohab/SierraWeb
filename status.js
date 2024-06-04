6a11e5b41314e22d9025378f929555176c33ea60
Mohab <124764172+D3vMohab@users.noreply.github.com>
2024-06-04 22:01:51 +0300

    Update status.js

--- status.js
+++ status.js
@@ -13,7 +13,7 @@ const response = await fetch('https://sierra.app/c.mobileprovision')
 	let formData = new FormData();
 	formData.append("mp", file);
 	// Upload MB to check status
-	const status = await fetch('https://ios.100-bt.cn/Index/check_p12', {method: "POST", body: formData});
+	const status = await fetch('https://api.100-bt.cn/checkcert', {method: "POST", body: formData});
 	const json = JSON.parse(await status.text());
 	if (json.state == "正常") {
 		document.getElementById("status").innerHTML = "Signed";
