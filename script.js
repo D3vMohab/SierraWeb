769b5f0cb472165b4072b5a9c188a018ea6619c3
Mohab <124764172+D3vMohab@users.noreply.github.com>
2024-06-12 14:16:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,102 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+	const installCountElement = document.getElementById('install-count');
+	let installCount = 0;
+
+	function updateInstallCount() {
+		installCount += Math.floor(Math.random() * 10) + 1;
+		installCountElement.textContent = installCount.toLocaleString();
+	}
+
+	setInterval(updateInstallCount, 1000);
+});
+
+
+function showNav() {
+
+	const nav = document.getElementById("nav-content");
+
+	if (nav.style.display == 'none' || nav.style.display == '') {
+
+		nav.style.display = 'grid';
+	} else {
+
+		nav.style.display = 'none';
+	}
+
+
+}
+
+function soon() {
+
+	alert("Comming soon!");
+}
+
+
+
+
+
+
+
+
+
+function download_popup() {
+	// Show Blacklist popup
+	var popup = document.getElementById("myPopup");
+	popup.classList.toggle("show");
+}
+
+
+
+
+function show_slider() {
+	document.getElementById('slider').style.animation = "slideIn 0.3s";
+	document.getElementById('s-con').style.animation = "fadeIn 1s";
+
+
+	setTimeout(() => {
+
+		document.getElementById('slider').style.width = "100%";
+
+	}, 300);
+
+
+
+
+}
+
+function dismiss() {
+	document.getElementById('slider').style.animation = "slideOut 0.3s";
+	document.getElementById('s-con').style.animation = "fadeOut 0.5s";
+
+	setTimeout(() => {
+
+		document.getElementById('slider').style.width = "0%";
+
+
+
+	}, 300)
+}
+
+
+document.addEventListener('touchmove', function(event) {
+	if (event.scale !== 1) {
+		event.preventDefault();
+	}
+}, {
+	passive: false
+});
+
+
+
+
+
+
+if (confirm('[SIERRA] Follow our Twitter for exciting giveaways and to stay updated with the latest news!')) {
+	// Save it!
+	window.open('https://x.com/SierraSigned', '_blank');
+} else {
+	// Do nothing!
+
+}
